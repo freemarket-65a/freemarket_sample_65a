a05fc7de1d6376dcf7bb2e057566f321addf6301
Sky-Onoppi <onoppisih_sky@yahoo.co.jp>
2019-12-23 17:02:43 +0900

    top-page-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # root to: 'users#profile'
-  # root to: 'users#mypage'
-  # root to: 'users#credit'
+  # root to: 'users#profile' ←仮で画面確認のため、残しています。
+  # root to: 'users#mypage' ←仮で画面確認のため、残しています。
+  # root to: 'users#credit' ←仮で画面確認のため、残しています。
   root to: 'products#index'
 end
