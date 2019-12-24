40563865f438d4387bf31e815ebe21067c7ba9f6
Sky-Onoppi <onoppisih_sky@yahoo.co.jp>
2019-12-24 21:40:40 +0900

    credit-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
 
-  # ↓作成途中のため、仮で残しています。
-  # root to: 'users#profile'
-  # root to: 'users#mypage'
-  # root to: 'users#credit_p'
-  # root to: 'users#credit'
-
   root to: 'products#index'
-
-  # ↓作成途中のため、仮で残しています。
-  # resources :products
 
   resources :users do
     member do
