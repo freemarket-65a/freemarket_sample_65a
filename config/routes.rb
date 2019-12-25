dc4e98c0edaf195ba521b6e8c83006c4f4e8cd9d
takuya12 <bucsojmfa12@gmail.com>
2019-12-25 17:17:31 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:'registration#complete'
+  root to: 'products#index'
+
+  resources :users do
+    member do
+      get 'mypage'
+      get 'profile'
+      get 'credit'
+      get 'logout'
+    end
+  end
+  resources :registration do
+    member do
+      get 'address'
+      get 'complete'
+      get 'login'
+      get 'member'
+      get 'payment'
+      get 'telephone'
+    end
+  end
+  resources :details
 end
