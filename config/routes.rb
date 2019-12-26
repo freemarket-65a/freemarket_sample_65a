2dea46afa74b87f560de1b9338f9f2fd1d82db85
Sky-Onoppi <onoppisih_sky@yahoo.co.jp>
2019-12-26 16:50:34 +0900

    identity-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  # root to: 'users#profile' ←仮で画面確認のため、残しています。
-  # root to: 'users#mypage' ←仮で画面確認のため、残しています。
-  # root to: 'users#credit' ←仮で画面確認のため、残しています。
 
   root to: 'products#index'
 
@@ -15,6 +12,7 @@ Rails.application.routes.draw do
       get 'profile'
       get 'credit'
       get 'logout'
+      get 'identification'
     end
   end
 
