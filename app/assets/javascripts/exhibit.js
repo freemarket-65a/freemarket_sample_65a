f0b1e2d9da5f0ba2c72ba9a81635f317818a66fe
takuya12 <bucsojmfa12@gmail.com>
2020-01-10 14:53:59 +0900

    commit

--- app/assets/javascripts/exhibit.js
+++ app/assets/javascripts/exhibit.js
@@ -58,5 +58,16 @@ $(document).on('turbolinks:load', ()=> {
 });
 
 $('#article_lists').append("<%= escape_javascript(render @exhibits, object: @articles) %>");
-$("#view_more").replaceWith("<%= escape_javascript(
-link_to_next_page(@exhibits, 'もっと見る', remote: true, id: 'view_more')) %>");
+$("#view_more").replaceWith("<%= escape_javascript(link_to_next_page(@exhibits, 'もっと見る', remote: true, id: 'view_more')) %>");
+
+
+$('.slider').slick({
+    arrows:false,
+    asNavFor:'.thumb',
+});
+$('.thumb').slick({
+    asNavFor:'.slider',
+    focusOnSelect: true,
+    slidesToShow:5,
+    slidesToScroll:1
+});    
