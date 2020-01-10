b0733c20c650e298d01d6dac9192062c495be584
kazu4k <57794695+kazu4k@users.noreply.github.com>
2020-01-10 17:37:50 +0900

    comit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_083704) do
+ActiveRecord::Schema.define(version: 2020_01_07_100353) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
