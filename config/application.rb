b8df1e7c8a4d6cfe4af962de06c1ed0cf63c11f7
Sky-Onoppi <onoppisih_sky@yahoo.co.jp>
2020-01-15 21:03:59 +0900

    change-mini-1

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module FreemarketSample65a
 
     config.i18n.default_locale = :ja
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
