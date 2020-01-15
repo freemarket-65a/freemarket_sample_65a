9719bc65e1c7cb3652c8301bd51ad28f198bc15e
takuya12 <bucsojmfa12@gmail.com>
2020-01-15 18:40:11 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_card
 
   def mypage
   end
@@ -7,12 +8,24 @@ class UsersController < ApplicationController
   end
 
   def credit
+    
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
+    #保管した顧客IDでpayjpから情報取得
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+    @default_card_information = customer.cards.retrieve(@card.card_id)
   end
 
   def logout
   end
 
   def identification
+  end
+
+  private
+  
+  def set_card
+    @card = Card.find_by(user_id: current_user.id)
   end
 
 end
